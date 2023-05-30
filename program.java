40db5db9c218a060760eb79cae54b631c58de842
Aishwarya-kiran <aishwarya2824gowda@gmail.com>
2023-05-30 18:31:36 +0530

    push code

---
+++ program.java
@@ -0,0 +1,23 @@
+class Largestno
+{
+public static void main(String[] args)
+{
+		int a=3;
+		int b=6;
+		int c=5;
+		if(a>b)
+		{
+			if(a>c){
+			System.out.println(a);}
+		}else if(b>a){
+			if(b>c)
+			{
+			System.out.println(b);}
+		}else if(c>a)
+			{
+			if(c>b){
+			System.out.println(c);}
+		}
+	}
+
+}
